e83fff82c7e7654893a3e07d19373b77a671104f
Mat Harris <hurris88@gmail.com>
2019-06-08 13:32:58 -0700

    Cleanup OV pages

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -2,16 +2,19 @@
 
 @section('content')
   <section class="container mx-auto">
+    <h1 class="text-center mb-10">Here's all your Food Stuff</h1>
+
     <div class="md:w-1/2 mx-auto">
-      <h1 class="text-center">Here's all your Food Stuff</h1>
-  
       <ul class="text-center">
         @foreach ($food as $item)
-          <li>
-            <a href="/food/{{ $item->id }}" class="text-blue-600 font-semibold hover:underline">{{ $item->name }}</a>
+          <li class="my-5">
+            <a href="/food/{{ $item->id }}" class="btn">{{ $item->name }}</a>
           </li>
         @endforeach
       </ul>
+    </div>
+    <div class="md:w-1/2 mx-auto my-10 text-center">
+      <a href="/food/create" class="btn bg-green-700">Add +</a>
     </div>
   </section>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,14 @@
 
 @section('content')
 
-    <div class="container p-16 text-center">
+    <div class="container mx-auto p-16 text-center">
         <h1>Noms: A recipe & grocery project</h1>
         <p>What's for dinner?</p>
+        <nav class="py-10">
+            <ul class="flex justify-center">
+                <li><a href="/food" class="btn">Food Stuff</a></li>
+            </ul>
+        </nav>
     </div>
 
 @endsection
