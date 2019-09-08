ed896f0ab9edae6629961b7f0b809e9f709b3fe7
Mat Harris <matharris@sirrahtam.com>
2019-09-08 12:16:40 -0700

    Update header/homepage styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,31 +11,21 @@
         <ul class="flex justify-center my-5">
             <!-- Authentication Links -->
             @guest
-            <li class="mx-5">
-                <a class="btn" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-            <li class="mx-5">
-                <a class="btn" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            @endif
+                <li class="mx-5">
+                    <a class="btn" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                    <li class="mx-5">
+                        <a class="btn" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                @endif
             @else
-            <li class="mx-5">
-                <a href="/meals" class="btn"><span class="fas fa-utensils pr-3" aria-hidden="true"></span>Meals</a>
-            </li>
-            <li class="mx-5">
-                <a href="/food" class="btn"><span class="fas fa-list-ul pr-3" aria-hidden="true"></span>Pantry</a>
-            </li>
-            <li class="mx-5">
-                <a class="btn" href="{{ route('logout') }}"
-                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-            </li>
+                <li class="mx-5">
+                    <a href="/meals" class="btn"><span class="fas fa-utensils pr-3" aria-hidden="true"></span>Meals</a>
+                </li>
+                <li class="mx-5">
+                    <a href="/food" class="btn"><span class="fas fa-list-ul pr-3" aria-hidden="true"></span>Pantry</a>
+                </li>
             @endguest
         </ul>
     </nav>
--- resources/views/partials/layout/header.blade.php
+++ resources/views/partials/layout/header.blade.php
@@ -1,10 +1,24 @@
 <header>
   <nav>
-    <ul>
-      <li><a href="/" aria-label="Noms home">
-        <span class="fas fa-home pr-3" aria-hidden="true"></span>
-        Noms</a>
-      </li>    
+    <ul class="flex justify-between">
+      <li>
+        <a href="/" aria-label="Noms home" class="btn btn--home">
+          <span class="fas fa-home pr-3" aria-hidden="true"></span>
+          Noms
+        </a>
+      </li>
+      @auth
+        <li class="mx-5">
+          <a class="btn" href="{{ route('logout') }}"
+              onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+          </a>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
+        </li>
+      @endauth
     </ul>
   </nav>
 </header>
