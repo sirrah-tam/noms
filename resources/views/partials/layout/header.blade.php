b28f913f939793df42f2fea4b6eb67714466811d
Mat Harris <matharris@sirrahtam.com>
2020-03-22 20:15:50 -0700

    Add meals & pantry to header after logging in

--- resources/views/partials/layout/header.blade.php
+++ resources/views/partials/layout/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <nav>
-    <ul class="flex justify-between">
+    <ul class="flex">
       <li>
         <a href="/" aria-label="Noms home" class="btn btn--home">
           <span class="fas fa-home pr-3" aria-hidden="true"></span>
@@ -9,6 +9,14 @@
       </li>
       @auth
         <li class="mx-5">
+          <a href="/meals" class="btn"><span class="fas fa-utensils pr-3" aria-hidden="true"></span>Meals</a>
+        </li>
+        <li class="mx-5">
+          <a href="/food" class="btn"><span class="fas fa-list-ul pr-3" aria-hidden="true"></span>Pantry</a>
+        </li>
+      @endauth
+      @auth
+        <li class="mx-5 ml-auto">
           <a class="btn" href="{{ route('logout') }}"
               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
               {{ __('Logout') }}
